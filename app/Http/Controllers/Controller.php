ff5289e874f0ce54edf5282552a97e5dc5b1895e
Yogi Pranoto <yogi.yp52@gmail.com>
2021-01-27 08:58:34 +0700

    26/01/2021

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,19 +5,18 @@ namespace App\Http\Controllers;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use DB;
   
 class Controller extends BaseController
 {
     public function respondWithToken($token ,$currentUser)
     {
         $id = $currentUser->id;
-        $profile = User::all('');
+        $profile = DB::select('select * from users join user_profiles ON users.id = user_profiles.user_id where users.id='.$id);
         return response()->json([
             'token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => null,
-            'users' => $currentUser,
-            'profile' => $profile
+            // 'expires_in' => null,
+            'users' => $profile,
 
         ], 200);
     }
